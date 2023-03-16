8fe760ff829a42274cd8859289e623bd4cbf4cc4
JuanMaChicharra <juanmanuelgrajales1234@gmail.com>
2023-03-16 14:40:22 -0500

    finish

--- script/script.js
+++ script/script.js
@@ -11,23 +11,51 @@ function LeerData() {
     nombre = document.getElementById('nombre').value;
     correo = document.getElementById('correo').value;
     mensaje = document.getElementById('textarea').value;
-    console.log(nombre)
-    console.log(correo)
-    console.log(mensaje)
+   ValidarData(nombre,correo,mensaje)
+   GuardarLocalStorage(nombre,correo,mensaje)
+
 }
 
 function ValidarData(nombre,correo,mensaje) {
-    if(nombre.length==0    ||    correo.length==0   ||   mensaje.length==0){
-
-        Swal.fire({
-            title: "error",
-            Text : "do you want to continue",
-            icon : "error",
-            confirmButtonText: "cool",
-            iconColor: "purple",
-        })
-
-
-    }
+     if(nombre.length==0 || correo.length==0 || mensaje.length==0){
+        let timerInterval
+Swal.fire({
+    title: 'Porfavor completa los campos para enviar!',
+  html: 'Espera ⏰<b></b> ',
+  timer: 1500,
+  timerProgressBar: true,
+  didOpen: () => {
+    Swal.showLoading()
+    const b = Swal.getHtmlContainer().querySelector('b')
+    timerInterval = setInterval(() => {
+      b.textContent = Swal.getTimerLeft()
+    }, 100)
+  },
+  willClose: () => {
+    clearInterval(timerInterval)
+  }
+}).then((result) => {
+  /* Read more about handling dismissals below */
+  if (result.dismiss === Swal.DismissReason.timer) {
+    console.log('I was closed by the timer')
+  }
+  
+})
+     }
 }
+
+function GuardarLocalStorage(nombre,correo,mensaje) {
+    localStorage.setItem('Nombre',nombre)
+    localStorage.setItem('Correo',correo)
+    localStorage.setItem('Mensaje',mensaje)
+    ListarData()
+    }
+
+function ListarData() {
+    let nombreUsu = localStorage.getItem('Nombre')
+    let correoUsu = localStorage.getItem('Correo')
+    let mensajeUsu = localStorage.getItem('Mensaje')
+
+}
+
 
